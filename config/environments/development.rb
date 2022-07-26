d10e481c159d20b4af1d9ffa0a8ee3c257157f8e
EC2 Default User <ec2-user@ip-172-31-30-202.ap-northeast-1.compute.internal>
2022-07-26 13:31:52 +0000

    [finish]bookers2-bebug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,7 @@ Rails.application.configure do
   config.hosts << "49e69874be6d4ef3a0718e9b15430b98.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "e8e216492e184c9fb14c284ce4ca2c3d.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
